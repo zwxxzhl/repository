ab52f2d32615ec995cbebfbed434665abd11b0ab
zwxxzhl <csdhtt@foxmail.com>
2021-11-26 17:58:23 +0800

    .

--- doctor/pages/examine-view/examine-view.js
+++ doctor/pages/examine-view/examine-view.js
@@ -9,5 +9,14 @@ Page({
   },
   onShow: function() {
 
+  },
+  onNavigateTo: function (e) {
+    if ('1' === e.currentTarget.dataset.idx) {
+      wx.navigateTo({url: '/pages/index/index'});
+    } else if ('2' === e.currentTarget.dataset.idx) {
+
+    } else if ('3' === e.currentTarget.dataset.idx) {
+      wx.navigateTo({url: '/pages/high-web-we-v/high-web-we-v'});
+    }
   }
 })
