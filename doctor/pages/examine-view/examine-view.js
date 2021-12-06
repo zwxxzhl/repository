ac07a46901c86b64fed390e541e685bbd13cca4a
zwxxzhl <csdhtt@foxmail.com>
2021-12-06 14:19:55 +0800

    .

--- doctor/pages/examine-view/examine-view.js
+++ doctor/pages/examine-view/examine-view.js
@@ -14,7 +14,7 @@ Page({
     if ('1' === e.currentTarget.dataset.idx) {
       wx.navigateTo({url: '/pages/index/index'});
     } else if ('2' === e.currentTarget.dataset.idx) {
-
+      wx.navigateTo({url: '/pages/high-web-ex-v/high-web-ex-v'});
     } else if ('3' === e.currentTarget.dataset.idx) {
       wx.navigateTo({url: '/pages/high-web-we-v/high-web-we-v'});
     }
